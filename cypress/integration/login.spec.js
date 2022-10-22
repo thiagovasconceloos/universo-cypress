ad3134a36752f90ab291e09a0548534e22d230c0
Thiago Vasconcelos <vasconcelosthiago@outlook.com>
2022-10-21 22:50:32 -0300

    Técnicas para buscar e clicar em botões

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -7,7 +7,7 @@ it('Deve logar com sucesso', ()=>{
    cy.visit('https://training-wheels-qaninja.herokuapp.com/login')
    cy.get('#nickId').type('papitorocks')
    cy.get('#passId').type('pwd123')
-
+   cy.get('button[type=submit]').click()
 
 
 })
