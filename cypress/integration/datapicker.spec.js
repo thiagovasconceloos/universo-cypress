8d6e9da691edddcc02f01070f1871f4dbf350b46
Thiago Vasconcelos <vasconcelosthiago@outlook.com>
2022-10-22 19:03:51 -0300

    Adicionando Expressao Regular

--- cypress/integration/datapicker.spec.js
+++ cypress/integration/datapicker.spec.js
@@ -1,3 +1,4 @@
+const { date } = require("assert-plus")
 
 
 it('Deve selecionar a data de nascimento',()=>{
@@ -5,11 +6,12 @@ it('Deve selecionar a data de nascimento',()=>{
     const data = {
     mes: 'ago',
     ano: '1995',
-    dia: '27'
+    dia: '1'
+
 
     }
 
-
+  
 
   cy.visit('/datepicker')
   cy.get('.datetimepicker-dummy-input').click()
@@ -17,7 +19,7 @@ it('Deve selecionar a data de nascimento',()=>{
   cy.contains('.datepicker-month',data.mes).click()
   cy.get('.datepicker-nav-year').click()
   cy.contains('.datepicker-year span',data.ano).click()
-  cy.contains('button[class=date-item]',data.dia).click()
+  cy.contains('button[class=date-item]',new RegExp('^'+ data.dia + '$','g')).click()
 
 
 
