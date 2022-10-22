1531856a1bc8b0cbaf34aaa858e2b4f1119a0410
Thiago Vasconcelos <vasconcelosthiago@outlook.com>
2022-10-22 18:50:10 -0300

    Editando DatePicker

--- cypress/integration/datapicker.spec.js
+++ cypress/integration/datapicker.spec.js
@@ -1,15 +1,23 @@
 
 
 it('Deve selecionar a data de nascimento',()=>{
+ 
+    const data = {
+    mes: 'ago',
+    ano: '1995',
+    dia: '27'
+
+    }
+
 
 
   cy.visit('/datepicker')
   cy.get('.datetimepicker-dummy-input').click()
   cy.get('.datepicker-nav-month').click()
-  cy.contains('.datepicker-month','ago').click()
+  cy.contains('.datepicker-month',data.mes).click()
   cy.get('.datepicker-nav-year').click()
-  cy.contains('.datepicker-year span','1995').click()
-  cy.contains('button[class=date-item]',27).click()
+  cy.contains('.datepicker-year span',data.ano).click()
+  cy.contains('button[class=date-item]',data.dia).click()
 
 
 
