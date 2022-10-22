1e046fae12fee1ecab70cd0d99deea887e617020
Thiago Vasconcelos <vasconcelosthiago@outlook.com>
2022-10-22 18:32:40 -0300

    Adicionando no teste

--- cypress/integration/select2.spec.js
+++ cypress/integration/select2.spec.js
@@ -1,6 +1,14 @@
-it('Deve selecionar uma unica opcao',()=>{
+describe('Select 2',()=>{
+
+  before(()=>{
 
     cy.visit('/apps/select2/index.html')
+
+  })
+
+it('Deve selecionar uma unica opção',()=>{
+
+    
     cy.get('.select2-selection--single').click()
     cy.contains('.select2-results__option','Cypress').click()
    
@@ -8,3 +16,33 @@ it('Deve selecionar uma unica opcao',()=>{
  
  })
  
+ it('Deve selecionar  multiplas opções',()=>{
+    
+      const selecao = [
+        'Cypress',
+        'Robot Framework',
+        'Playwright'
+
+
+      ]
+
+      selecao.forEach((s)=>{
+
+        
+        cy.get('.select2-selection--multiple').click()
+        cy.contains('.select2-results__option',`${s}`).click()
+
+      })
+
+
+
+   
+
+  
+ 
+ 
+ })
+ 
+
+    
+})
