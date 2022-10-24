f8fb6b005cdbc45949e01ea6723170b39d6ef8ba
Thiago Vasconcelos <vasconcelosthiago@outlook.com>
2022-10-24 17:27:03 -0300

    Toast (notificações flutuantes

---
+++ cypress/integration/toast.spec.js
@@ -0,0 +1,18 @@
+
+ it('Deve exibir uma notificação toast',()=>{
+
+    cy.visit('/toaster')
+    cy.contains('button','Toast Rápido').click()
+
+    cy.get('.notification')
+    .should('be.visible')
+    .should('have.text','Essa notificação é muito rápida!')
+    // cy.get('body')
+    
+    // cy.wait(3000)
+
+ 
+ 
+ 
+ 
+ })
