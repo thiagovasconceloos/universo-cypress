096de3cf910cedccc82a4ff1dd231dded99f2605
Thiago Vasconcelos <vasconcelosthiago@outlook.com>
2022-10-24 16:53:57 -0300

    Javascript Prompt Stubs)

--- cypress/integration/alerts.spec.js
+++ cypress/integration/alerts.spec.js
@@ -14,3 +14,54 @@ it('Deve exibir uma mensagem de alerta',()=>{
 
 
 })
+
+it('Deve confirmar a solicitacao',()=>{
+
+  cy.visit('/javascript_alerts')
+
+  cy.contains('button','Confirma').click()
+
+  cy.on('window:confirm',()=> true )
+  cy.get('#result').should('have.text','Mensagem confirmada')
+
+
+
+})
+
+
+it('Deve cancelar a solicitacao',()=>{
+
+  cy.visit('/javascript_alerts')
+
+  cy.contains('button','Confirma').click()
+
+  cy.on('window:confirm',()=> false )
+  cy.get('#result').should('have.text','Mensagem não confirmada')
+
+
+
+})
+
+
+
+it('Deve exibir mensagem de boas vindas',()=>{
+
+  cy.visit('/javascript_alerts')
+
+  cy.window().then(($win) => { 
+
+    cy.stub($win, 'prompt').returns('Thiago')
+    cy.contains('button','Prompt').click()
+
+})
+
+
+
+  
+
+  
+  cy.get('#result').should('have.text','Olá, Thiago')
+
+
+
+})
